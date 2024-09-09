09954ee81b1c96fe56d0977d376cac77d792a339
wolfingten <jagu00005@stud.uni-saarland.de>
2024-09-09 21:03:18 +0200

    add scripts for inference and evaluation

--- weakly_supervised_parser/utils/evaluate.sh
+++ weakly_supervised_parser/utils/evaluate.sh
@@ -2,4 +2,4 @@ python3 -m nltk.downloader stopwords
 
 export PYTHONPATH=/nethome/jguertler/weakly-supervised-parsing/
 
-python weakly_supervised_parser/tree/compare_trees.py --tree2 /data/users/jguertler/outputs/ws_parser/inference.txt
+python /nethome/jguertler/weakly-supervised-parsing/weakly_supervised_parser/tree/compare_trees.py --tree2 /data/users/jguertler/outputs/ws_parser/inference.txt
--- weakly_supervised_parser/utils/inference.sh
+++ weakly_supervised_parser/utils/inference.sh
@@ -2,7 +2,7 @@ python3 -m nltk.downloader stopwords
 
 export PYTHONPATH=/nethome/jguertler/weakly-supervised-parsing/
 
-python ./../weakly_supervised_parser/inference.py \
+python /nethome/jguertler/weakly-supervised-parsing/weakly_supervised_parser/inference.py \
     --use_inside \
     --model_name_or_path roberta-base \
     --inside_max_seq_length 256 \
