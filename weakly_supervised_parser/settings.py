f33ffabecd3b19dd658fbb189c910bad8007a55f
wolfingten <jagu00005@stud.uni-saarland.de>
2024-10-06 19:56:49 +0200

    import error

--- weakly_supervised_parser/settings.py
+++ weakly_supervised_parser/settings.py
@@ -39,13 +39,25 @@ PTB_TEST_GOLD_WITHOUT_PUNCTUATION_PATH = (
     PTB_TREES_ROOT_DIR + "test/test_trees_without_punct.txt"
 )
 
-# PTB_TRAIN_GOLD_WITHOUT_PUNCTUATION_ALIGNED_PATH = PTB_TREES_ROOT_DIR + "ptb-train-gold-without-punctuation-aligned.txt"
-# PTB_VALID_GOLD_WITHOUT_PUNCTUATION_ALIGNED_PATH = PTB_TREES_ROOT_DIR + "ptb-valid-gold-without-punctuation-aligned.txt"
-# PTB_TEST_GOLD_WITHOUT_PUNCTUATION_ALIGNED_PATH = PTB_TREES_ROOT_DIR + "ptb-test-gold-without-punctuation-aligned.txt"
-#
-# YOON_KIM_TRAIN_GOLD_WITHOUT_PUNCTUATION_PATH = PTB_TREES_ROOT_DIR + "Yoon_Kim/ptb-train-gold-filtered.txt"
-# YOON_KIM_VALID_GOLD_WITHOUT_PUNCTUATION_PATH = PTB_TREES_ROOT_DIR + "Yoon_Kim/ptb-valid-gold-filtered.txt"
-# YOON_KIM_TEST_GOLD_WITHOUT_PUNCTUATION_PATH = PTB_TREES_ROOT_DIR + "Yoon_Kim/ptb-test-gold-filtered.txt"
+PTB_TRAIN_GOLD_WITHOUT_PUNCTUATION_ALIGNED_PATH = (
+    PTB_TREES_ROOT_DIR + "ptb-train-gold-without-punctuation-aligned.txt"
+)
+PTB_VALID_GOLD_WITHOUT_PUNCTUATION_ALIGNED_PATH = (
+    PTB_TREES_ROOT_DIR + "ptb-valid-gold-without-punctuation-aligned.txt"
+)
+PTB_TEST_GOLD_WITHOUT_PUNCTUATION_ALIGNED_PATH = (
+    PTB_TREES_ROOT_DIR + "ptb-test-gold-without-punctuation-aligned.txt"
+)
+
+YOON_KIM_TRAIN_GOLD_WITHOUT_PUNCTUATION_PATH = (
+    PTB_TREES_ROOT_DIR + "Yoon_Kim/ptb-train-gold-filtered.txt"
+)
+YOON_KIM_VALID_GOLD_WITHOUT_PUNCTUATION_PATH = (
+    PTB_TREES_ROOT_DIR + "Yoon_Kim/ptb-valid-gold-filtered.txt"
+)
+YOON_KIM_TEST_GOLD_WITHOUT_PUNCTUATION_PATH = (
+    PTB_TREES_ROOT_DIR + "Yoon_Kim/ptb-test-gold-filtered.txt"
+)
 
 # Predictions
 PTB_SAVE_TREES_PATH = "/data/users/jguertler/models/ws_parser/ger/"
