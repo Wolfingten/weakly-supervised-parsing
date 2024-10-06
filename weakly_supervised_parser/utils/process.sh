b053480b8dc29fba5da4e00637f7d2d3c929cd2b
wolfingten <jagu00005@stud.uni-saarland.de>
2024-10-06 19:39:49 +0200

    add entry script

---
+++ weakly_supervised_parser/utils/process.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+export PYTHONPATH=/nethome/jguertler/weakly-supervised-parsing/
+
+python /home/wolfingten/projects/weakly-supervised-parsing/weakly_supervised_parser/utils/pre_process.py
