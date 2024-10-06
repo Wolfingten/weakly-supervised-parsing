d6963f73e0bfaf74bbdcb072a7d562509ae028c3
wolfingten <jagu00005@stud.uni-saarland.de>
2024-10-06 19:45:49 +0200

    fix file paths

--- weakly_supervised_parser/utils/process.sh
+++ weakly_supervised_parser/utils/process.sh
@@ -2,4 +2,4 @@
 
 export PYTHONPATH=/nethome/jguertler/weakly-supervised-parsing/
 
-python /home/wolfingten/projects/weakly-supervised-parsing/weakly_supervised_parser/utils/pre_process.py
+python /nethome/jguertler/weakly-supervised-parsing/weakly_supervised_parser/utils/pre_process.py
