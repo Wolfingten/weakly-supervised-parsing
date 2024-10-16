0215544f903dba33f3c7025107b882c0978546bf
wolfingten <jagu00005@stud.uni-saarland.de>
2024-10-16 13:41:28 +0200

    prerpocessing should be correct

--- weakly_supervised_parser/settings.py
+++ weakly_supervised_parser/settings.py
@@ -1,7 +1,7 @@
-PROJECT_DIR = "/home/wolfingten/projects/weakly_supervised_parser/"
+PROJECT_DIR = "/nethome/jguertler/weakly_supervised_parser/"
 
-PTB_TREES_ROOT_DIR = "/home/wolfingten/projects/data/spmrl/GERMAN_SPMRL/gold/ptb/"
-PTB_SENTENCES_ROOT_DIR = "/home/wolfingten/projects/data/spmrl/GERMAN_SPMRL/gold/ptb/"
+PTB_TREES_ROOT_DIR = "/data/users/jguertler/datasets/spmrl/GERMAN_SPMRL/gold/ptb/"
+PTB_SENTENCES_ROOT_DIR = "/data/users/jguertler/datasets/spmrl/GERMAN_SPMRL/gold/ptb/"
 
 PTB_TRAIN_SENTENCES_WITH_PUNCTUATION_PATH = (
     PTB_SENTENCES_ROOT_DIR + "train/train.German.gold.ptb.tobeparsed.raw"
@@ -24,13 +24,13 @@ PTB_TEST_SENTENCES_WITHOUT_PUNCTUATION_PATH = (
 )
 
 PTB_TRAIN_GOLD_WITH_PUNCTUATION_PATH = (
-    PTB_TREES_ROOT_DIR + "train/train_gold_with_punct.txt"
+    PTB_TREES_ROOT_DIR + "train/train_trees_with_punct.txt"
 )
 PTB_VALID_GOLD_WITH_PUNCTUATION_PATH = (
-    PTB_TREES_ROOT_DIR + "dev/dev_gold_with_punct.txt"
+    PTB_TREES_ROOT_DIR + "dev/valid_trees_with_punct.txt"
 )
 PTB_TEST_GOLD_WITH_PUNCTUATION_PATH = (
-    PTB_TREES_ROOT_DIR + "test/test_gold_with_punct.txt"
+    PTB_TREES_ROOT_DIR + "test/test_trees_with_punct.txt"
 )
 
 PTB_TRAIN_GOLD_WITHOUT_PUNCTUATION_PATH = (
@@ -64,7 +64,7 @@ YOON_KIM_TEST_GOLD_WITHOUT_PUNCTUATION_PATH = (
 )
 
 # Predictions
-PTB_SAVE_TREES_PATH = "/home/wolfingten/projects/outputs/ws_parser/ger/"
+PTB_SAVE_TREES_PATH = "/data/users/jguertler/models/ws_parser/ger/"
 
 # Training
-TRAINED_MODEL_PATH = "/home/wolfingten/projects/models/ws_parser/ger/"
+TRAINED_MODEL_PATH = "/data/users/jguertler/models/ws_parser/ger/"
