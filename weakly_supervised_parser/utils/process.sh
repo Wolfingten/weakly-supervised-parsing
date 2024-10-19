41de4a989d19f02deee6427065ed761ae561a583
wolfingten <jagu00005@stud.uni-saarland.de>
2024-10-19 22:29:10 +0200

    set correct paths

--- weakly_supervised_parser/utils/process.sh
+++ weakly_supervised_parser/utils/process.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-export PYTHONPATH=/home/wolfingten/projects/weakly-supervised-parsing/
+export PYTHONPATH=/nethome/jguertler/weakly-supervised-parsing/
 
 python /nethome/jguertler/weakly-supervised-parsing/weakly_supervised_parser/utils/process_spmrl.py
