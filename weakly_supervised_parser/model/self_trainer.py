c89d827acc024220a37b29c750f94d89b0c76ae9
wolfingten <jagu00005@stud.uni-saarland.de>
2024-10-27 02:43:29 +0200

    wandb logging

--- weakly_supervised_parser/model/self_trainer.py
+++ weakly_supervised_parser/model/self_trainer.py
@@ -5,13 +5,24 @@ from weakly_supervised_parser.settings import TRAINED_MODEL_PATH
 
 
 class SelfTrainingClassifier:
-    def __init__(self, inside_model, num_iterations: int = 5, prob_threshold: float = 0.99):
+    def __init__(
+        self, inside_model, num_iterations: int = 5, prob_threshold: float = 0.99
+    ):
         self.inside_model = inside_model
         self.num_iterations = num_iterations
         self.prob_threshold = prob_threshold
 
     def fit(
-        self, train_inside, valid_inside, train_batch_size, eval_batch_size, learning_rate, max_epochs, dataloader_num_workers, scale_axis, predict_batch_size
+        self,
+        train_inside,
+        valid_inside,
+        train_batch_size,
+        eval_batch_size,
+        learning_rate,
+        max_epochs,
+        dataloader_num_workers,
+        scale_axis,
+        predict_batch_size,
     ):  # -1 for unlabeled
         inside_strings = train_inside["sentence"].values
         labels = train_inside["label"].values
@@ -33,20 +44,38 @@ class SelfTrainingClassifier:
             filename="inside_model_self_trained_base",
         )
 
-        self.inside_model.load_model(pre_trained_model_path=TRAINED_MODEL_PATH + f"inside_model_self_trained_base.onnx")
+        self.inside_model.load_model(
+            pre_trained_model_path=TRAINED_MODEL_PATH
+            + f"inside_model_self_trained_base.onnx"
+        )
 
-        unlabeledy = self.inside_model.predict(pd.DataFrame(dict(sentence=unlabeled_inside_strings)), scale_axis, predict_batch_size)
-        unlabeledprob = self.inside_model.predict_proba(pd.DataFrame(dict(sentence=unlabeled_inside_strings)), scale_axis, predict_batch_size)
+        unlabeledy = self.inside_model.predict(
+            pd.DataFrame(dict(sentence=unlabeled_inside_strings)),
+            scale_axis,
+            predict_batch_size,
+        )
+        unlabeledprob = self.inside_model.predict_proba(
+            pd.DataFrame(dict(sentence=unlabeled_inside_strings)),
+            scale_axis,
+            predict_batch_size,
+        )
         unlabeledy_old = []
         # re-train, labeling unlabeled instances with model predictions, until convergence
         idx = 0
-        while (len(unlabeledy_old) == 0 or np.any(unlabeledy != unlabeledy_old)) and idx < self.num_iterations:
+        while (
+            len(unlabeledy_old) == 0 or np.any(unlabeledy != unlabeledy_old)
+        ) and idx < self.num_iterations:
             unlabeledy_old = np.copy(unlabeledy)
-            uidx = np.where((unlabeledprob[:, 0] > self.prob_threshold) | (unlabeledprob[:, 1] > self.prob_threshold))[0]
+            uidx = np.where(
+                (unlabeledprob[:, 0] > self.prob_threshold)
+                | (unlabeledprob[:, 1] > self.prob_threshold)
+            )[0]
 
             train_df_concat = pd.DataFrame(
                 dict(
-                    sentence=np.concatenate((labeled_inside_strings, unlabeled_inside_strings[uidx])),
+                    sentence=np.concatenate(
+                        (labeled_inside_strings, unlabeled_inside_strings[uidx])
+                    ),
                     label=np.hstack((labeledy, unlabeledy_old[uidx])),
                 )
             )
@@ -64,16 +93,26 @@ class SelfTrainingClassifier:
                 learning_rate=learning_rate,
                 max_epochs=max_epochs,
                 dataloader_num_workers=dataloader_num_workers,
-#                scale_axis = scale_axis,
-#                predict_batch_size = predict_batch_size,
+                #                scale_axis = scale_axis,
+                #                predict_batch_size = predict_batch_size,
                 outputdir=TRAINED_MODEL_PATH,
                 filename=filename,
             )
 
-            self.inside_model.load_model(pre_trained_model_path=TRAINED_MODEL_PATH + f"{filename}.onnx")
+            self.inside_model.load_model(
+                pre_trained_model_path=TRAINED_MODEL_PATH + f"{filename}.onnx"
+            )
 
-            unlabeledy = self.inside_model.predict(pd.DataFrame(dict(sentence=unlabeled_inside_strings)), scale_axis, predict_batch_size)
-            unlabeledprob = self.inside_model.predict_proba(pd.DataFrame(dict(sentence=unlabeled_inside_strings)), scale_axis, predict_batch_size)
+            unlabeledy = self.inside_model.predict(
+                pd.DataFrame(dict(sentence=unlabeled_inside_strings)),
+                scale_axis,
+                predict_batch_size,
+            )
+            unlabeledprob = self.inside_model.predict_proba(
+                pd.DataFrame(dict(sentence=unlabeled_inside_strings)),
+                scale_axis,
+                predict_batch_size,
+            )
             idx += 1
 
         return self
